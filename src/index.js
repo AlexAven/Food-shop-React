6e7e16c272b62a88015b1c67985ad8d04eb520d4
Alex Kazakov <gloom31@gmail.com>
2023-12-31 20:23:57 +0300

    routing

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/Food-shop'>
+    <BrowserRouter basename='/hacker-news-combinator'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
